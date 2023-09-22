b2b8d5d6db233d564fb483d250d02df92b9ac6e0
andreinavega <andrevesu16@gmail.com>
2023-09-21 22:52:45 -0300

    Added dark mode option with localStorage persistance. Added refresh quote functionality

--- assets/script.js
+++ assets/script.js
@@ -2,61 +2,141 @@ const GOTQuotes = {
     api: 'https://api.gameofthronesquotes.xyz/v1/random',
     quoteWrapper: document.querySelector('#quote-wrapper'),
     quoteContainer: document.querySelector('#quote'),
-
-    async init() {
-        setTimeout(async () => {
-            this.showQuote(await this.getRandomQuote());
-        }, 500);
-    },
-
+    modeContainer: document.querySelector('#mode'),
+    mode: localStorage.getItem('mode'),
+    loading: false,
+    
     async getRandomQuote() {
         const response = await fetch(this.api);
         const quote = await response.json();
         return quote;
     },
 
-    showQuote(quote) {
-        let index = 0;
-        const { sentence } = quote;
-        const len = sentence.length;
-        const tm = 2000 / len * Math.sqrt(len) / 7;
+    async init() {
+        this.loading = true;
+        setTimeout(async () => {
+            await this.showQuote(await this.getRandomQuote());
+            this.loading = false;
+        }, 500);
 
-        this.adjustContainer(sentence);
+        this.setMode();
+        this.handleQuoteRefresh();
+    },
 
-        const intervalId = setInterval(() => {
-            const prev = sentence[index - 1];
-            const char = sentence[index++];
+    handleQuoteRefresh() {
+        this.quoteContainer.addEventListener('click', async () => {
+            // Do nothing if loading another quote
+            if (this.loading) return;
+            this.loading = true;
+            await this.cleanUp();
+            
+            await this.showQuote(await this.getRandomQuote());
+            
+            this.loading = false;
+        });
+    },
 
-            if (!char) {
-                this.showAuthor(quote.character);
-                return clearInterval(intervalId);
-            }
+    setMode() {
+        const img = this.createImg();
+        this.setModeIcon(img);
+        this.setModeClass();
 
-            // Avoid breaking words while typing... work on this later
-            if (prev === ' ') {
-                const container = this.quoteContainer;
-                const { height } = container.getBoundingClientRect();
-                const span = document.createElement('span');
-                let word = '';
-                for (let char of sentence.slice(index - 1)) {
-                    if (char === ' ') break;
-                    word += char;
+        this.modeContainer.addEventListener('click', () => {
+            this.mode = this.mode === 'dark' ? 'light' : 'dark';
+            this.setModeIcon(img);
+            this.setModeClass();
+            localStorage.setItem('mode', this.mode);
+        });
+    },
+
+    createImg() {
+        const img = document.createElement('img');
+        this.setModeIcon(img);
+        this.modeContainer.appendChild(img);
+        return img;
+    },
+
+    setModeIcon(img) {
+        const icon = this.mode === 'dark' ? 'light' : 'dark'
+        img.src = `/assets/img/moon-${icon}.png`;
+    },
+
+    setModeClass() {
+        document.body.classList.toggle('dark', this.mode === 'dark');
+    },
+
+    cleanUp() {
+        return new Promise(resolve => {
+            const chars = this.quoteContainer.querySelectorAll('span');
+            let index = 0;
+    
+            const intervalID = setInterval(() => {
+                chars[index++].classList.add('fade-out');
+    
+                if (index === chars.length) {
+                    clearInterval(intervalID);
+                    this.quoteWrapper.querySelector('.author').classList.add('fade-out');
+    
+                    setTimeout(() => {
+                        // ...
+                        document.querySelector('.author')?.remove();
+                        this.quoteWrapper.removeAttribute('style');
+                        this.quoteContainer.innerHTML = '';
+    
+                        resolve();
+                    }, 500);
                 }
-                span.innerText = word;
-                container.appendChild(span);
-                const { height: newHeight } = container.getBoundingClientRect();
+            }, 20);
+        });
+    },
 
-                if (height !== newHeight) {
-                    console.log('br')
-                    container.appendChild(document.createElement('br'));
+    showQuote(quote, callback) {
+        return new Promise(resolve => {
+            let index = 0;
+            const { sentence } = quote;
+            const len = sentence.length;
+            const tm = 2000 / len * Math.sqrt(len) / 7;
+    
+            this.adjustContainer(sentence);
+    
+            const intervalId = setInterval(() => {
+                const prev = sentence[index - 1];
+                const char = sentence[index++];
+    
+                if (!char) {
+                    this.showAuthor(quote.character);
+                    if (typeof callback === 'function') {
+                        callback();
+                    }
+                    resolve();
+                    return clearInterval(intervalId);
                 }
-
-                span.remove();
-            }
-
-            // Add char
-            this.addChar(char);
-        }, tm);
+    
+                // Avoid breaking words while typing... work on this later
+                if (prev === ' ') {
+                    const container = this.quoteContainer;
+                    const { height } = container.getBoundingClientRect();
+                    const span = document.createElement('span');
+                    let word = '';
+                    for (let char of sentence.slice(index - 1)) {
+                        if (char === ' ') break;
+                        word += char;
+                    }
+                    span.innerText = word;
+                    container.appendChild(span);
+                    const { height: newHeight } = container.getBoundingClientRect();
+    
+                    if (height !== newHeight) {
+                        container.appendChild(document.createElement('br'));
+                    }
+    
+                    span.remove();
+                }
+    
+                // Add char
+                this.addChar(char);
+            }, tm);
+        })
     },
 
     addChar(char) {
@@ -93,3 +173,10 @@ const GOTQuotes = {
 };
 
 GOTQuotes.init();
+
+/*
+GOTQuotes.cleanUp()
+GOTQuotes.showQuote({ sentence: 'Las funciones se llaman cómo? Con paréntesis', character: { name: 'Sabio anónimo' }}, () => {
+    setTimeout(alert, 1000, 'Frase sabia mostrada.')
+})
+*/
